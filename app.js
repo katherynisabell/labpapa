6c5e05d421d3cee8b8150e0d696040acaf440759
Cumbie <bcumbie@una.edu>
2023-02-23 16:20:02 -0600

    added delete endpoint and refactor get to slash

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-require('dotenv').config()
+require("dotenv").config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;  
 const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
+const PORT = process.env.PORT || 3000;
+const herokuVar = process.env.HEROKU_NAME || "local Barry"
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+// const MONGO_URI = "mongodb+srv://barry:GMSk9usexg5A8p5Q@cluster0.taug6.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
 
-app.set('view engine', 'ejs');
 
 async function cxnDB(){
 
@@ -30,11 +33,20 @@ async function cxnDB(){
 }
 
 
-app.get('/', (req, res) => {
-  //res.send('Hello World This is Barry 3! <br/> <a href="mongo">mongo</a>');
+// app.get('/', (req, res) => {
+//   //res.send('Hello World This is Barry 3! <br/> <a href="mongo">mongo</a>');
 
-  res.render('index'); 
+//   res.render('index'); 
 
+// })
+
+app.get('/', async (req, res) => {
+
+  let result = await cxnDB().catch(console.error); 
+
+  // console.log("get/: ", result);
+
+  res.render('index', {  drinkData : result })
 })
 
 app.get('/mongo', async (req, res) => {
@@ -49,21 +61,62 @@ app.get('/mongo', async (req, res) => {
 
 })
 
-app.get('/update', async (req, res) => {
+// app.get('/create', async (req, res) => {
 
-  //get data from the form 
+//   //get data from the form 
 
-  console.log("in get to slash update:", req.query.ejsFormName); 
-  myName = req.query.ejsFormName; 
+//   console.log("in get to slash update:", req.query.ejsFormName); 
+//   myName = req.query.ejsFormName; 
 
-  //update in the database. 
-  client.connect; 
-  const collection = client.db("chillAppz").collection("drinkz");
-  await collection.insertOne({ 
-    drink_name: "coldiessss from the render app"
+//   //update in the database. 
+//   client.connect; 
+//   const collection = client.db("chillAppz").collection("drinkz");
+//   await collection.insertOne({ 
+//     drink_name: "coldiessss from the render app"
+// })
+
+// })
+
+app.post('/addDrink', async (req, res) => {
+
+  try {
+    // console.log("req.body: ", req.body) 
+    client.connect; 
+    const collection = client.db("chillAppz").collection("drinkz");
+    await collection.insertOne(req.body);
+      
+    res.redirect('/');
+  }
+  catch(e){
+    console.log(error)
+  }
+  finally{
+   // client.close()
+  }
+
 })
 
+
+app.post('/deleteDrink/:id', async (req, res) => {
+
+  try {
+    console.log("req.parms.id: ", req.params.id) 
+    
+    client.connect; 
+    const collection = client.db("chillAppz").collection("drinkz");
+    let result = await collection.findOneAndDelete( { _id: new ObjectId( req.params.id) })
+    .then(result => {
+      console.log(result); 
+      res.redirect('/');
+    })
+    .catch(error => console.error(error))
+  }
+  finally{
+    //client.close()
+  }
+
 })
+
 
 console.log('in the node console');
 
