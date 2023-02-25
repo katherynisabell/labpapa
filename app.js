532962cfca7d1685dab4b43bf80e7d0ccae54cc4
Nicole Reilly <nreilly@una.edu>
2023-02-24 19:33:08 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { ObjectId } = require('mongodb')
 const PORT = process.env.PORT || 3000;
 const herokuVar = process.env.HEROKU_NAME || "local Barry"
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const MONGO_URI = "mongodb+srv://barry:nb3amjtQWhSN6ibH@cluster0.taug6.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URI = "mongodb+srv://nreilly:vY4A1eYR0sVUpISS@cluster0.6kyiorf.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
